b37da1c883ff643616f0390ed76bc8ce21166247
jebediahelliott <jebediahelliott@gmail.com>
2019-04-05 21:04:28 -0400

    Start playing with math

--- c-sharp-playground/Program.cs
+++ c-sharp-playground/Program.cs
@@ -6,33 +6,15 @@ namespace c_sharp_playground
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 5; i++)
-            {
-                //Console.WriteLine($"first loop i = {i}");
-                for (int j = 1; j <= 4; j++)
-                {
-                    Console.Write('0');
-                    //Console.WriteLine($"second loof j = {j}");
-                }
-                Console.WriteLine();
-            }
-
-            for (int i = 0; i <= 100; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j <= i; j++)
-                {
-                    Console.Write('0');
-                }
-                Console.WriteLine();
-            }
+            double x = 3.23456;
+            // ceiling
+            Console.WriteLine(Math.Ceiling(x));
+            // floor
+            Console.WriteLine(Math.Floor(x));
+            // round
+            Console.WriteLine(Math.Round(x, 2));
+            // truncate
+            Console.WriteLine(Math.Truncate(x, ));
         }
     }
 }
